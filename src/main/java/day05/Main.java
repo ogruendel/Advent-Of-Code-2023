7ddd9493abe7e1519ec4995bd22f4190e7aa673e
Oliver Gründel <oliver.gruendel2004@gmail.com>
2023-12-08 18:25:49 +0100

    Day 05 Part 2; Horrible runtime

--- src/main/java/day05/Main.java
+++ src/main/java/day05/Main.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<String> input = List.of(File.stringFromFile("src/main/java/day05/input.txt").split("\\n"));
-        List<String> test = List.of(File.stringFromFile("src/main/java/day05/test.txt").split("\\n"));
 
         System.out.println(Almanac.solvePart1(input));
+        System.out.println(Almanac.solvePart2(input));
     }
 }
